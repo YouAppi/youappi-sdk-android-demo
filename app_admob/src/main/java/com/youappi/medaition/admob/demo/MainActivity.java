202a54cddfc9a1e4ad0c5faaeb82eb6840c725dd
YANAY-LP\Yanay Zimran <yanay.z@youappi.com>
2018-03-26 10:27:50 +0300

    addition of moat indication in simple demo app

--- app_admob/src/main/java/com/youappi/medaition/admob/demo/MainActivity.java
+++ app_admob/src/main/java/com/youappi/medaition/admob/demo/MainActivity.java
@@ -3,10 +3,12 @@ package com.youappi.medaition.admob.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.youappi.ai.sdk.YouAPPi;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     MobileAds.initialize(this, ADMOB_APP_ID);
     rewardedVideo = MobileAds.getRewardedVideoAdInstance(this);
+
+    TextView versionStr = (TextView) findViewById(R.id.versionStr);
+    versionStr.setText("SDK version: " + YouAPPi.getInstance().getVersionStr());
+
+    TextView moatState = (TextView) findViewById(R.id.moat_state);
+//    if (YouAppiMopub.isMoat()){
+//      moatState.setText("Trackers: Moat");
+//    } else {
+//      moatState.setText("Trackers: None");
+//    }
   }
 
   @Override
