67f4019979f658ff5d3d8b4e586a9cbe3ca6cc61
YANAY-LP\Yanay Zimran <yanay.z@youappi.com>
2018-04-04 15:45:30 +0300

    ui tweeks

--- app-demo-admob/src/main/java/com/youappi/medaition/admob/demo/MainActivity.java
+++ app-demo-admob/src/main/java/com/youappi/medaition/admob/demo/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import com.youappi.ai.sdk.YouAPPi;
 import com.youappi.mediation.admob.YouAppiInterstitialAd;
 import com.youappi.mediation.admob.YouAppiInterstitialVideo;
@@ -23,15 +27,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String ADMOB_UNIT_ID_INTERSTITIAL_VIDEO = "ca-app-pub-9942448998483932/3243450527";
 
     private Button loadRewardedVideo;
-    private Button showRewardedVideo;
     private Button loadInterstitialVideo;
-    private Button showInterstitialVideo;
     private Button loadInterstitialAd;
+    private Button showInterstitialVideo;
+    private Button showRewardedVideo;
     private Button showInterstitialAd;
+
+
+    private View spinner;
+
+    private boolean rewardedShown = false;
 
     private RewardedVideoAd rewardedVideo;
     private InterstitialAd interstitialAd;
     private InterstitialAd interstitialVideo;
+
+    private RewardedVideoAdListener rewardedVideoAdListener = new RewardedVideoAdListener() {
+        @Override
+        public void onRewardedVideoAdLoaded() {
+            showSpinner(false);
+            updateUiState();
+        }
+
+        @Override
+        public void onRewardedVideoAdOpened() {
+            rewardedShown = true;
+            updateUiState();
+        }
+
+        @Override
+        public void onRewardedVideoStarted() {
+            rewardedShown = true;
+            updateUiState();
+        }
+
+        @Override
+        public void onRewardedVideoAdClosed() {
+            rewardedShown = true;
+            updateUiState();
+        }
+
+        @Override
+        public void onRewarded(RewardItem rewardItem) {
+
+        }
+
+        @Override
+        public void onRewardedVideoAdLeftApplication() {
+
+        }
+
+        @Override
+        public void onRewardedVideoAdFailedToLoad(int i) {
+            showSpinner(false);
+            updateUiState();
+            showToast("Failed to load");
+        }
+
+        @Override
+        public void onRewardedVideoCompleted() {
+            rewardedShown = true;
+            updateUiState();
+        }
+    };
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private AdListener adListener = new AdListener() {
+        @Override
+        public void onAdFailedToLoad(int i) {
+            showSpinner(false);
+            updateUiState();
+            showToast("Failed to load");
+        }
+
+        @Override
+        public void onAdLoaded() {
+            showSpinner(false);
+            updateUiState();
+        }
+
+        @Override
+        public void onAdClosed() {
+            updateUiState();
+        }
+
+        @Override
+        public void onAdOpened() {
+            updateUiState();
+        }
+
+        @Override
+        public void onAdClicked() {
+            updateUiState();
+        }
+
+        @Override
+        public void onAdImpression() {
+            updateUiState();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +148,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         loadInterstitialAd.setOnClickListener(this);
         showInterstitialAd.setOnClickListener(this);
 
+        spinner = findViewById(R.id.spinner_layout);
+        spinner.setVisibility(View.GONE);
+
         MobileAds.initialize(this, ADMOB_APP_ID);
         rewardedVideo = MobileAds.getRewardedVideoAdInstance(this);
 
@@ -64,6 +164,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    } else {
 //      moatState.setText("Trackers: None");
 //    }
+        updateUiState();
     }
 
     @Override
@@ -71,16 +172,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.load_rewarded:
                 rewardedVideo.loadAd(ADMOB_REWARDED_VIDEO_ADUNIT_ID, new AdRequest.Builder().build());
+                rewardedVideo.setRewardedVideoAdListener(rewardedVideoAdListener);
+                rewardedShown = false;
+                showSpinner(true);
                 break;
-
             case R.id.show_rewarded:
                 rewardedVideo.show();
                 break;
             case R.id.load_interstitial_video:
                 interstitialVideo = new InterstitialAd(this);
+                interstitialVideo.setAdListener(adListener);
                 interstitialVideo.setAdUnitId(ADMOB_UNIT_ID_INTERSTITIAL_VIDEO);
                 Bundle videoBundle = new Bundle();
                 interstitialVideo.loadAd(new AdRequest.Builder().addCustomEventExtrasBundle(YouAppiInterstitialVideo.class, videoBundle).build());
+                showSpinner(true);
                 break;
             case R.id.show_interstitial_video:
                 if (interstitialVideo != null && interstitialVideo.isLoaded()) {
@@ -90,9 +195,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.load_interstitial_ad:
                 interstitialAd = new InterstitialAd(this);
+                interstitialAd.setAdListener(adListener);
                 interstitialAd.setAdUnitId(ADMOB_UNIT_ID_INTERSTITIAL_AD);
                 Bundle bundle = new Bundle();
                 interstitialAd.loadAd(new AdRequest.Builder().addCustomEventExtrasBundle(YouAppiInterstitialAd.class, bundle).build());
+                showSpinner(true);
                 break;
             case R.id.show_interstitial_ad:
                 if (interstitialAd != null && interstitialAd.isLoaded()) {
@@ -101,5 +208,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    private void showSpinner(boolean showSpinner) {
+        if (spinner != null) {
+            spinner.setVisibility(showSpinner ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    private void updateUiState() {
+        showInterstitialAd.setEnabled(interstitialAd != null && interstitialAd.isLoaded());
+        showInterstitialVideo.setEnabled(interstitialVideo != null && interstitialVideo.isLoaded());
+        showRewardedVideo.setEnabled(rewardedVideo.isLoaded() && !rewardedShown);
     }
 }
