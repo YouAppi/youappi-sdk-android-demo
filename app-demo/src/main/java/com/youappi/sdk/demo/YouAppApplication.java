38529d2b6885a9eec1514f6d76d13c5186f3fa6c
Rachel Hia <>
2019-05-20 16:27:17 +0300

    set consent and gdpr to final

--- app-demo/src/main/java/com/youappi/sdk/demo/YouAppApplication.java
+++ app-demo/src/main/java/com/youappi/sdk/demo/YouAppApplication.java
@@ -13,12 +13,12 @@ public class YouAppApplication extends Application {
 
   // Please note this is YouAppi's demo access token. It should be replaced with your app access token.
   private static final String DEMO_TOKEN = "821cfa77-3127-42b5-9e6b-0afcecf77c67";
+  private static final boolean consent = true;
+  private static final boolean gdpr = true;
 
   @Override
   public void onCreate() {
     super.onCreate();
-    boolean consent = true;
-    boolean gdpr = true;
     YouAPPi.init(this, DEMO_TOKEN, consent, gdpr);
   }
 }
