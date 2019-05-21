be68cc9328294361941b711bc6675cd69407616f
Rachel Hia <>
2019-05-21 10:46:50 +0300

    changed gdpr to false

--- app-demo/src/main/java/com/youappi/sdk/demo/YouAppApplication.java
+++ app-demo/src/main/java/com/youappi/sdk/demo/YouAppApplication.java
@@ -14,7 +14,7 @@ public class YouAppApplication extends Application {
   // Please note this is YouAppi's demo access token. It should be replaced with your app access token.
   private static final String DEMO_TOKEN = "821cfa77-3127-42b5-9e6b-0afcecf77c67";
   private static final boolean consent = true;
-  private static final boolean gdpr = true;
+  private static final boolean gdpr = false;
 
   @Override
   public void onCreate() {
